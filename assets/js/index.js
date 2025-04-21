688ee84a2fa67b9e6e69e3a24e3a6c0c166a7cdc
AmitKapilaCodeIns <amitkapila@hotmail.com>
2025-04-21 14:03:50 +0100

    index.js fetch JSON data/cards.json

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,15 @@
+let cards = [];
+let firstCard, secondCard;
+let lockBoard = false;
+let score = 0;
 
+// document.querySelector(".score").textContent = score;
+
+fetch("./data/cards.json") //Fetching the cards data from a local JSON file
+    .then((res) => res.json()) //Parsing the JSON data
+    .then((data) => {
+        //Using the data to create the cards
+        cards = [...data, ...data]; // spread operator to duplicate the array
+    });
+
+console.log(cards);
